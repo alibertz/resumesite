c3b4042daf27d2726be820524e8f1cede43c0790
Kevin <kevinaliberti@gmail.com>
2019-06-30 16:22:23 -0400

    Updates

---
+++ precache-manifest.6a7671c59b69d9c3bfcdd5a3da65bb9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9aadb28883d68f5824c329869eacbe2",
+    "url": "/resumesite/index.html"
+  },
+  {
+    "revision": "e06938f1f672a877bd98",
+    "url": "/resumesite/static/css/main.d383e060.chunk.css"
+  },
+  {
+    "revision": "d2d82d68c7df91bb776f",
+    "url": "/resumesite/static/js/2.e703e8cc.chunk.js"
+  },
+  {
+    "revision": "e06938f1f672a877bd98",
+    "url": "/resumesite/static/js/main.23fbddfd.chunk.js"
+  },
+  {
+    "revision": "0b976761a1a07ab2bff7",
+    "url": "/resumesite/static/js/runtime~main.6645f16d.js"
+  },
+  {
+    "revision": "7dff06d012401f1cbfcd7b709895b09d",
+    "url": "/resumesite/static/media/bg.7dff06d0.jpg"
+  }
+]);
